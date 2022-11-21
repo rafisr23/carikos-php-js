5c41e4f44d9e066ed22926fd93d852723f164427
Abdur Rafi <rafiridwan23@gmail.com>
2022-11-21 11:35:22 +0700

    database config

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,10 @@
 <?php
 // change this to your own path
-define('BASEURL', 'http://pemweb-project-kost-nativephp.local/public/');
+define('BASEURL', 'http://phpmvc-template/public/');
 
 // DATABASE
 // change this to your own database
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'pemweb-project-kost-nativephp');
+define('DB_NAME', 'phpmvc-template');
