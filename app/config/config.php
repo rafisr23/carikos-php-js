e7185a5e1625287a9a9630f5d7f94cdcd24368c0
Abdur Rafi <rafiridwan23@gmail.com>
2022-11-25 19:13:33 +0700

    init commit

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,10 @@
 <?php
 // change this to your own path
-define('BASEURL', 'http://phpmvc-template/public/');
+define('BASEURL', 'http://pemweb-project-uts2/public/');
 
 // DATABASE
 // change this to your own database
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'phpmvc-template');
+define('DB_NAME', 'pemweb-project-uts2');
