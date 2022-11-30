9b876a40cf46ab802ccda1fb7af839a4f1a4efb9
Abdur Rafi <rafiridwan23@gmail.com>
2022-11-30 15:01:51 +0700

    minor update view home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,4 +1,6 @@
 <!-- insert your website body -->
 <div class="Page Title">
   <h1>Nanti Halaman Overview ada disini ya</h1>
+  <h3><a href="<?= BASEURL; ?>/users">TABEL USERS</a></h3>
+  <h3><a href="<?= BASEURL; ?>/owners">TABEL OWNERS</a></h3>
 </div>
